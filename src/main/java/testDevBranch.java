012955fe354ca13259ac32b68c1f984108361c10
lucas <1903076902@qq.com>
2020-07-06 01:37:53 +0800

    new file

---
+++ src/main/java/testDevBranch.java
@@ -0,0 +1,9 @@
+/**
+ * @author lucas
+ * @create 2020-07-06-01:37
+ */
+public class testDevBranch {
+    public static void main(String[] args) {
+        System.out.println("new dev branch");
+    }
+}
