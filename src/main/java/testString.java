5db1b9bdcb596f15ac5ae9b3890758893ae75a5a
lucas <1903076902@qq.com>
2020-07-06 00:36:07 +0800

    create testString.java file

---
+++ src/main/java/testString.java
@@ -0,0 +1,13 @@
+import redis.clients.jedis.Jedis;
+
+/**
+ * @author lucas
+ * @create 2020-07-06-00:32
+ */
+public class testString {
+    public static void main(String[] args) {
+        Jedis jedis = new Jedis("localhost", 6379);
+        jedis.set("key1", "001");
+
+    }
+}
